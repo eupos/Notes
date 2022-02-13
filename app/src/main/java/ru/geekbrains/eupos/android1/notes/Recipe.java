f17f9da8c9e9e2419e226b0f0a0b5188c5805874
Eupos <eupos@mail.ru>
2022-02-13 21:11:50 +0300

    Начал создавать класс

---
+++ app/src/main/java/ru/geekbrains/eupos/android1/notes/Recipe.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.eupos.android1.notes;
+
+public class Recipe {
+    private String recipeName;
+    private String recipeAuthor;
+    private int dateLoaded;
+    private String recipeCountry;
+    private String recipeIngredient;
+}
