bae0b80401b39be641d356fc20fe2bfaa1b852cf
Eupos <eupos@mail.ru>
2022-02-14 20:20:08 +0300

    Добавил методы onViewCreated

--- app/src/main/java/ru/geekbrains/eupos/android1/notes/ingredientsRecipe.java
+++ app/src/main/java/ru/geekbrains/eupos/android1/notes/ingredientsRecipe.java
@@ -2,6 +2,8 @@ package ru.geekbrains.eupos.android1.notes;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -23,5 +25,10 @@ public class ingredientsRecipe extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ingredients_recipe, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
--- app/src/main/java/ru/geekbrains/eupos/android1/notes/titleRecipe.java
+++ app/src/main/java/ru/geekbrains/eupos/android1/notes/titleRecipe.java
@@ -2,6 +2,8 @@ package ru.geekbrains.eupos.android1.notes;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -22,5 +24,10 @@ public class titleRecipe extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_title_recipe, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
